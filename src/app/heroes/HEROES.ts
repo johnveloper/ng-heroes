02b9f288572fb66449a5716677278400d355c990
johnveloper <johnveloper@gmail.com>
2019-03-31 17:40:06 +0200

    Add heroes array

---
+++ src/app/heroes/HEROES.ts
@@ -0,0 +1,20 @@
+import Hero from './Hero';
+
+
+
+const HEROES: Hero[] = [
+  { id: 0, name: 'Tornado' },  
+  { id: 1, name: 'Mr. Nice' },
+  { id: 2, name: 'Narco' },
+  { id: 3, name: 'Bombasto' },
+  { id: 4, name: 'Celeritas' },
+  { id: 5, name: 'Magneta' },
+  { id: 6, name: 'RubberMan' },
+  { id: 7, name: 'Dynama' },
+  { id: 8, name: 'Dr IQ' },
+  { id: 9, name: 'Magma' }
+];
+
+
+
+export default HEROES;
