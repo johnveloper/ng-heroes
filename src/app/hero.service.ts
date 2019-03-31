4fbe94cf7aa5733ce93cbde1e39c39b5c572a181
johnveloper <johnveloper@gmail.com>
2019-03-31 18:43:50 +0200

    Format 'hero' service files

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
-export class HeroService {
 
-  constructor() { }
+
+
+export class HeroService {
+  constructor() {}
 }
