5e48d9589ea7b81cddbe7aa7f45f364864ecf340
johnveloper <johnveloper@gmail.com>
2019-03-31 18:41:24 +0200

    Show the app-hero-detail component within app-heroes

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import Hero from '../heroes/Hero';
 
 
 
@@ -11,6 +12,7 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
   constructor() {}
   ngOnInit() {}
 }
