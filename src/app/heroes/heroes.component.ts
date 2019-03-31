57fc4cc8e7df6b7c1a296ee9b8e90ac97d48387e
johnveloper <johnveloper@gmail.com>
2019-03-31 16:47:03 +0200

    Explain the 'heroes' component class file, add 'hero' property and show it in template

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, // import the Component symbol to annotate the component class with @Component
+  OnInit // lifecycle hook, called shortly after creating a component, good place to put initialization logic
+} from '@angular/core';
 
 
 
+// @Component is a decorator fuction that specifies the Angular metadata for the component
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.sass']
+  selector: 'app-heroes', // Component's CSS element selector
+  templateUrl: './heroes.component.html', // Location of the component template file
+  styleUrls: ['./heroes.component.sass'] // Location of styles for this component (scoped, private)
 })
 
 
 
 export class HeroesComponent implements OnInit {
+
+  // properties
+  hero = 'Windstorm';
+
   constructor() {}
-  ngOnInit() {}
+  ngOnInit() {} // lifecycle hook, called shortly after creating a component, good place to put initialization logic
 }
